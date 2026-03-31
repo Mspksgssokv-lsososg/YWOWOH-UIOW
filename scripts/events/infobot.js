3b23fcabcc809c5ebcfdd08d3d59e535a810b876
Mspksgssokv-lsososg <itssiddik487@gmail.com>
2026-03-31 12:07:00 +0600

    Update infobot.js

--- scripts/events/infobot.js
+++ scripts/events/infobot.js
@@ -1,8 +1,14 @@
 module.exports = {
-    onChat: async ({ bot, msg }) => {
-        if (msg.new_chat_member && msg.new_chat_member.id === bot.id) {
-            const infoMessage = `Hello, DEAR !\n╭━━━━━━ [ I'm ${bot.username} ] ━━━━━━╮\n\n, the bot. How can I assist you today?`;
-            bot.sendMessage(msg.chat.id, infoMessage);
-        }
+  run: async ({ bot, event }) => {
+
+    if (event.new_chat_member && event.new_chat_member.id === bot.id) {
+      const infoMessage = `Hello, DEAR !
+╭━━━━━━ [ I'm ${bot.username} ] ━━━━━━╮
+
+I'm the bot. How can I assist you today?`;
+
+      bot.sendMessage(event.chat.id, infoMessage);
     }
+
+  }
 };
