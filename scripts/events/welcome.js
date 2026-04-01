ecbbc3324336ab038ee33b7923e95e55591535dc
Mspksgssokv-lsososg <itssiddik487@gmail.com>
2026-04-01 12:02:40 +0600

    Update welcome.js

--- scripts/events/welcome.js
+++ scripts/events/welcome.js
@@ -1,21 +1,26 @@
 module.exports = {
-  run: async ({ bot, event }) => {
+  config: {
+    name: "welcome",
+    author: "SK-SIDDIK-KHAN",
+    version: "1.0",
+    role: 0
+  },
 
+  onChat: async ({ bot, event }) => {
     if (event.new_chat_members) {
-      const newMembers = event.new_chat_members
-        .map(member => member.first_name)
-        .join(', ');
+      const names = event.new_chat_members
+        .map(u => u.first_name)
+        .join(", ");
 
-      const chatName = event.chat.title || 'this group';
+      const groupName = event.chat.title || "this group";
 
-      const welcomeMessage = `𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐃𝐄𝐀𝐑: 
-╭──────────────────╮
- ✨ ${newMembers} 🍀
-╰──────────────────╯ to
- ⑅⃝»̶̶͓͓͓̽̽̽»̶̶͓͓͓̽̽̽๓ ${chatName} ꕀ⃘⃜⃟ؖؖؖؖؖؖؖؖؖꙮ͌͌͌͌͌͌͌͌͌͌͌͌͌͌ꔹ⃟ꔹ⃟!`;
+      const msg = `𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐃𝐄𝐀𝐑:
+╭───────────────╮
+✨ ${names}
+╰───────────────╯
+📍 ${groupName}`;
 
-      bot.sendMessage(event.chat.id, welcomeMessage);
+      bot.sendMessage(event.chat.id, msg);
     }
-
   }
 };
