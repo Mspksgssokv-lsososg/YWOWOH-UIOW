c26c65bad82a14a890a7c5e397eff351275198ea
Mspksgssokv-lsososg <itssiddik487@gmail.com>
2026-04-01 12:17:59 +0600

    Update welcome.js

--- scripts/events/welcome.js
+++ scripts/events/welcome.js
@@ -1,25 +1,18 @@
 module.exports = {
-  config: {
-    name: "welcome",
-    version: "1.0",
-    author: "SK-SIDDIK-KHAN"
-  },
+  event: "message",
 
-  onChat: async ({ bot, event }) => {
-    if (event.new_chat_members) {
+  run: async ({ bot, event }) => {
 
-      const names = event.new_chat_members
-        .map(u => u.first_name)
-        .join(", ");
+    if (!event.new_chat_members) return;
 
-      const group = event.chat.title || "Group";
+    const names = event.new_chat_members.map(u => u.first_name).join(", ");
+    const group = event.chat.title || "Group";
 
-      bot.sendMessage(event.chat.id,
+    bot.sendMessage(event.chat.id,
 `╭━━━〔 😎 SIDDIK BOT 〕━━━╮
 ┃ 👋 Welcome ${names}
-┃ 🏡 Group: ${group}
+┃ 🏡 ${group}
 ╰━━━━━━━━━━━━━━━╯`
-      );
-    }
+    );
   }
 };
